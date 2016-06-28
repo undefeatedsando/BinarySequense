b53ae6bdb85a914b1e2c61f4ef0f689ebbd373a3
Сергей <deadelf79@users.noreply.github.com>
2016-06-28 23:31:40 +0300

    Комментарии и выравнивание

--- BinSeq.cpp
+++ BinSeq.cpp
@@ -1,35 +1,51 @@
 class Separator
-{private:
+{
+private:
 int inputCount;
 int outputCount;
 int *start_element;
-int *end
+int *end;
 int *current_element;
+	
 public:
+  /* Конструктор класса Separator
+  
+  из каждых 'inputCount' элементов должны быть отправлены дальше только 'outputCount'
+  */
   Separator(int &input, int &output, int *data):inputCount(input),outputCount(output),current_element(data), start_element(data), end(data+inputCount-1);
+  
+  /* Сброс */
   void Reset(unsigned int newinput=0, unsigned int newoutput=0);
 	{
-	inputCount=newinput;
-	outputCount=newoutput;
+	  inputCount=newinput;
+	  outputCount=newoutput;
 	}
+  /* Проверка валидности
+  функция Valid говорит, попадает текущий элемент в выходной поток или нет
+  */
   bool Valid() const;
 	{
-	if ((current_element-start_element)%outputCount==0)
-	return true;
-	else
-	return false;
+	  if ((current_element-start_element)%outputCount==0)
+	    return true;
+	  else
+	    return false;
 	}
+	
+  /* Переход на следующий элемент
+  итерация по методу Next переходит к следующему элементу
+  если уже достигнут последний вариант, переходим к первому, но возвращаем false.
+  */ 
   bool Next();
 	{
-	if (current_element==end)
-	{
-	current_element=start_element;
-	return false;
-	}
-	else
-	{
-	current_element++;
-	return true;
-	}
+	  if (current_element==end)
+	  {
+	    current_element=start_element;
+	    return false;
+	  }
+	  else
+	  {
+	    current_element++;
+	    return true;
+	  }
 	}
 };
